fce8450ae8d5c79158681cc8ceaef266d74a7753
Hristian-N <144681383+Hristian-N@users.noreply.github.com>
2023-09-11 16:10:12 +0300

    Update Program.cs

--- UserSystem/Program.cs
+++ UserSystem/Program.cs
@@ -135,6 +135,7 @@ namespace UserSystem
 
             Console.ForegroundColor = ConsoleColor.DarkGreen;
             Console.WriteLine(message);
+            Console.WriteLine("That's all folks");
         }
     }
 }
